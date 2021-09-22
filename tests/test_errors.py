8d5edc59e9c1fcd31a6ad8509c962f370b19cb86
Ittigorn Tradussadee <eternal.fondness@gmail.com>
2021-09-22 17:07:35 +0700

    Added tests

--- tests/test_errors.py
+++ tests/test_errors.py
@@ -6,3 +6,7 @@ client = TestClient(app)
 def test_wrong_path():
   response = client.get('/wrong_path')
   assert response.status_code == 404
+
+def test_bad_format_username():
+  response = client.get('/users/สวัสดี')
+  assert response.status_code == 422
--- tests/test_main.py
+++ tests/test_main.py
@@ -14,3 +14,15 @@ def test_status():
   assert response.status_code == 200
   assert 'root_dir'         in response.json().keys()
   assert 'environment'      in response.json().keys()
+
+
+def test_get_tweets_by_a_hashtag():
+  response = client.get('/hashtags/python')
+  assert response.status_code == 200
+  assert type(response.json()) is list
+
+
+def test_get_tweets_by_a_hashtag():
+  response = client.get('/users/twitter')
+  assert response.status_code == 200
+  assert type(response.json()) is list
